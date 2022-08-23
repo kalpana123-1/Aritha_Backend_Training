2748f92683b7b53fa05129cbb15feccc89c91025
kalpana123-1 <kalpanatamatam1822000@gmail.com>
2022-08-23 22:14:23 +0530

    Commit on post request

--- crudapi/routes/books.js
+++ crudapi/routes/books.js
@@ -14,4 +14,20 @@ router.get('/', async(req, res)=>{
     }
 })
 
+router.post('/', async(req, res){
+    const book = new Book({
+        book_name : req.body.book_name,
+        author_name: req.body.author_name,
+        year_of_publish: req.body.year_of_publish,
+        no_of_publish : req.body.no_of_publish,
+        visit : req.body.visit
+    })
+
+    try{
+        const b = await book.save();
+        res.json(b);
+    }
+
+})
+
 module.exports = router;
